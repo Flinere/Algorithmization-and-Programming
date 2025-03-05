f67413629f05d750020d45ac3a095f7ffb014fa0
Денис <103769099+Flinere@users.noreply.github.com>
2025-03-05 18:55:50 +0300

    Add files via upload

---
+++ dz6.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Напишите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("система счисления с 2 до 9");
+int q1 = Convert.ToInt32(Console.ReadLine());
+while (q1 < 2 || q1 > 9)
+{
+    Console.WriteLine("Введите правильную систему счисления");
+            q1 = Convert.ToInt32(Console.ReadLine());
+}
+int i = 0;
+int otvet = 0;
+while (true)
+{
+    int seki = num % 10;
+    if (seki >= q1)
+    {
+        return;
+        Console.WriteLine(seki + "невходит в данную систему исчисления");
+    }
+
+    otvet += seki * (int)Math.Pow(q1, i);
+    i++;
+
+    if (num < 10)
+    {
+        Console.WriteLine(otvet);
+        break;
+    }
+    num = num / 10;
+}
+  
